bf95258262fabe48b7fbb36c08321c03a03a0823
HuyQuang2601 <huyxauzai2601@gmail.com>
2024-11-26 00:49:47 +0700

    Add files via upload

--- Session05_Ex5.c
+++ Session05_Ex5.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    for (int i = 1; i <= 10; i++) {
+	int a,i;
+    for ( i = 1; i <= 10; i++) {
         printf("Bang cuu chuong %d:\n", i);
-    for (int a = 1; a <= 10; a++) {
+    for ( a = 1; a <= 10; a++) {
         printf("%d x %d = %d\n", i, a, i * a);
         }
 
