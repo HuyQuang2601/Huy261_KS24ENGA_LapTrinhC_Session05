b96dea53aaa54d1fd1415004a6a4d1e5bfae35eb
HuyQuang2601 <huyxauzai2601@gmail.com>
2024-11-26 08:08:08 +0700

    Add files via upload

--- Session05_Ex7.c
+++ Session05_Ex7.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
 
 int main(){
-	int a,b,c,lonNhat;
-	printf("Nhap 2 so nguyen duong: ");
-	scanf("%d %d",a,b);
-	while(b != 0){
-		b=c;
-		a%b == b;
-		a=c;
-			if(a%b==0){
-		printf("Uoc chung lon nhat cua %d va %d la: %d ", a,b,lonNhat);
-
+	int a,b,i,min;
+	printf("Nhap 2 so nguyen: ");
+	scanf("%d %d", &a,&b);
+	min = a < b ? a : b;
+	for(int i = min; i>=1; i--){
+		if ((a%i==0) && (b%i==0)){
+			printf("UCLN cua %d va %d la %d", a, b, i);
+			break;
+		}
 	}
-}
-
 }
